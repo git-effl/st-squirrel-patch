bd9b812274a39a0455b68b21233baac2e0f30c67
git-effl <serenatamagnini@gmail.com>
2026-09-08 12:21:39 +0200

    Add files via upload

---
+++ squirrel_Export.h
@@ -0,0 +1,12 @@
+#ifndef SQUIRREL_EXPORT_H
+#define SQUIRREL_EXPORT_H
+
+#ifndef SQUIRREL_EXPORT
+#  define SQUIRREL_EXPORT
+#endif
+
+#ifndef SQUIRREL_NO_EXPORT
+#  define SQUIRREL_NO_EXPORT
+#endif
+
+#endif
